956497b954b27454ddcd49dad461b327a6935e01
Trevor Gunn <tgunn@live.ca>
2024-05-12 15:05:11 -0400

    Move geo regex to library, improve a = -1 case

--- js/ogf.js
+++ js/ogf.js
@@ -11,4 +11,10 @@ function geo(a, r) {
     const r_text = (r < 0 ? "+" : "-") + (abs_r == 1 ? "" : abs_r);
     return (a < 0 ? "-" : "") + "\\frac{" + Math.abs(a) + "}{1" + r_text + "x}";
 }
+function geoRe(a, r) {
+    const a_re = a == 1 ? "" : (a == -1 ? "-\\s*" : a + "\\s*\\*?\\s*");
+    const ratio_re = r == 1 ? "" : (r > 0 ? r + "\\^n" : "\\(" + r + "\\)\\^n");
+    const re = new RegExp(a == 1 && r == 1 ? "^\\s*1\\s*$" : "^\\s*" + a_re + ratio_re + "\\s*$");
+    return re;
+}
 //# sourceMappingURL=ogf.js.map
