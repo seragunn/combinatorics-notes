12540d02952d2880618c3a1071695dca54c2b48c
Trevor Gunn <tgunn@live.ca>
2024-05-12 16:01:35 -0400

    Add random sequence question

--- src/ogf.ts
+++ src/ogf.ts
@@ -15,9 +15,29 @@ function geo(a: number, r: number): string {
     return (a < 0 ? "-" : "") + "\\frac{" + Math.abs(a) + "}{1" + r_text + "x}";
 }
 
-function geoRe(a: number, r: number): RegExp {
+function geoRE(a: number, r: number): RegExp {
     const a_re = a == 1 ? "" : (a == -1 ? "-\\s*" : a + "\\s*\\*?\\s*");
     const ratio_re = r == 1 ? "" : (r > 0 ? r + "\\^n" : "\\(" + r + "\\)\\^n");
     const re = new RegExp(a == 1 && r == 1 ? "^\\s*1\\s*$" : "^\\s*" + a_re + ratio_re + "\\s*$");
     return re;
+}
+
+function seq(a: number, an: number, r: number, rn: number): string {
+    let seq = "0,".repeat(an);
+    for (let i = 0; i < Math.floor((10 - an)/rn); i++) {
+        seq += a * r**i;
+        seq += "," + "0,".repeat(rn - 1);
+    }
+    return seq;
+}
+
+function monomialRE(coef: number, exp: number): RegExp {
+    if (exp == 0) {
+        return new RegExp("^\\s*" + coef + "\\s*$");
+    }
+    let re = "^\\s*";
+    re += coef == 1 ? "" : (coef == -1 ? "-\\s*" : coef + "\\s*\\*?\\s*");
+    re += exp == 1 ? "x" : "x\\^" + exp;
+    re += "\\s*$";
+    return new RegExp(re);
 }
