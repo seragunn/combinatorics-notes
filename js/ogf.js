5dbf72489d90a1e86d512a8ad3caec00990de185
Sera Gunn <seragunn@outlook.com>
2026-01-24 22:33:50 -0500

    Built site for gh-pages

--- js/ogf.js
+++ js/ogf.js
@@ -12,8 +12,8 @@ function geo(a, r) {
     return (a < 0 ? "-" : "") + "\\frac{" + Math.abs(a) + "}{1" + r_text + "x}";
 }
 function geoRE(a, r) {
-    const a_re = a == 1 ? "" : (a == -1 ? "-\\s*" : a + "\\s*\\*?\\s*");
-    const ratio_re = r == 1 ? "" : (r > 0 ? r + "\\^n" : "\\(" + r + "\\)\\^n");
+    const a_re = a == 1 ? "" : a == -1 ? "-\\s*" : a + "\\s*\\*?\\s*";
+    const ratio_re = r == 1 ? "" : r > 0 ? r + "\\^n" : "\\(" + r + "\\)\\^n";
     const re = new RegExp(a == 1 && r == 1 ? "^\\s*1\\s*$" : "^\\s*" + a_re + ratio_re + "\\s*$");
     return re;
 }
@@ -30,7 +30,7 @@ function monomialRE(coef, exp) {
         return new RegExp("^\\s*" + coef + "\\s*$");
     }
     let re = "^\\s*";
-    re += coef == 1 ? "" : (coef == -1 ? "-\\s*" : coef + "\\s*\\*?\\s*");
+    re += coef == 1 ? "" : coef == -1 ? "-\\s*" : coef + "\\s*\\*?\\s*";
     re += exp == 1 ? "x" : "x\\^" + exp;
     re += "\\s*$";
     return new RegExp(re);
